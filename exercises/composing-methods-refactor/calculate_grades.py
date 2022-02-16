6166e299e0d30aec3d14b9f4dccf0a5bb5a9e63c
Edison Li <edison4354@gmail.com>
2022-02-15 16:52:51 -0800

    Refactoring: Exercise 1 (Completed!)

--- exercises/composing-methods-refactor/calculate_grades.py
+++ exercises/composing-methods-refactor/calculate_grades.py
@@ -1,28 +1,40 @@
-# Written by Kamran Bigdely
-# Example for Compose Methods: Extract Method.
+"""This module provides access to the mathematical functions defined by the C standard"""
 import math
 
+
+def find_mean(all_grades):
+    """Return the mean of grades"""
+    total = 0
+    for grade in all_grades:
+        total += grade
+    return total / len(all_grades)
+
+
+def standard_deviation(all_grades, mean):
+    """Return the standard deviation of grades"""
+    sum_of_sqrs = 0
+    for grade in all_grades:
+        sum_of_sqrs += (grade - mean) ** 2
+    return math.sqrt(sum_of_sqrs / len(all_grades))
+
+
 def print_stat():
+    """Prints out the mean and standard deviation"""
     grade_list = []
     # Get the inputs from the user
     n_student = 5
     for _ in range(n_student):
         grade_list.append(int(input('Enter a number: ')))
 
-    # Calculate the mean and standard deviation of the grades
-    total = 0 # Do you think 'sum' is a good var name? Run pylint to figure out!
-    for grade in grade_list:
-        total += grade
-    mean = total / len(grade_list)
-    sd = 0 # standard deviation
-    sum_of_sqrs = 0
-    for grade in grade_list:
-        sum_of_sqrs += (grade - mean) ** 2
-    sd = math.sqrt(sum_of_sqrs / len(grade_list))
-    # print out the mean and standard deviation in a nice format.
+    # Calculates mean and stand deciation
+    mean = find_mean(grade_list)
+    s_deviation = standard_deviation(grade_list, mean)
+
+    # prints out the mean and standard deviation in a nice format
     print('****** Grade Statistics ******')
     print("The grades's mean is:", mean)
-    print('The population standard deviation of grades is: ', round(sd, 3))
+    print('The population standard deviation of grades is: ', round(s_deviation, 3))
     print('****** END ******')
+
 
 print_stat()
